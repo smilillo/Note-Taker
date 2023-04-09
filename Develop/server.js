4ca36536627b983906793a8542e32546b2273f0b
smilillo <sofiemilillo@gmail.com>
2023-04-08 20:40:53 -0400

    imported express.js and routes files to server.js

---
+++ Develop/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3001;
+
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
