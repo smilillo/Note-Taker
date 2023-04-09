7f746ac76cdd6dbc7e018bc32f5235fe4094076f
smilillo <sofiemilillo@gmail.com>
2023-04-08 21:01:47 -0400

    added listen port to server js

--- Develop/server.js
+++ Develop/server.js
@@ -7,3 +7,6 @@ const PORT = 3001;
 const apiRoutes = require('./routes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes.js');
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
