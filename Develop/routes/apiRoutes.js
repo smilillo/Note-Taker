38cadc81c51209e822adf33ae4974bb52c523dbe
smilillo <sofiemilillo@gmail.com>
2023-04-10 08:56:32 -0400

    post route for api/notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -9,7 +9,30 @@ app.get('/api/notes', (req, res) => {
 });
 // POST /api/notes
 app.post('/api/notes', (req, res) => {
+    console.info(`POST request received to add notes`);
     
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
 });
 // DELETE /api/notes
 app.delete('/api/notes', (req, res) => {
