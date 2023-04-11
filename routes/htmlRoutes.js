a1afb284ddd8fa0d176932c559be7dc9944c653f
smilillo <sofiemilillo@gmail.com>
2023-04-11 19:10:00 -0400

    fixed /notes path, needed to be placed above wildcard

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const app = require ('express').Router();
 
-// GET homepage route - wildcard route
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-);
-
 // GET notes route
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+// GET homepage route - wildcard route
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 module.exports = app;
